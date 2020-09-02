dfb291a01a6e5ab29e2fadfd315e26aa679dc1c7
Konstantin Novikov <nov.konstantin@gmail.com>
2020-09-02 18:33:06 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,49 @@
+import { Component, ViewChild, OnInit, ElementRef } from '@angular/core';
+import { NgxCameraComponent, CameraImage } from 'ngx-camera';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+
+  title = 'camera-flip';
+
+  videoWidth: 640;
+  videoHeight: 480;
+
+  @ViewChild('ngxCam', { static: true})
+  ngxCam: NgxCameraComponent;
+
+  @ViewChild('canvas', { static: true})
+  canvas: ElementRef;
+
+  capturedImage = null;
+
+  ngOnInit(): void {
+    console.log();
+  }
+
+  start(): void {
+  }
+
+  flip(): void {
+  }
+
+  screenshot(): void {
+    this.ngxCam.takeSnapshot();
+  }
+
+  onScreenshot(image: CameraImage): void {
+    this.capturedImage = image.imageAsBase64;
+  }
+
+  stop(): void {
+    this.ngxCam.stopMediaTracks();
+  }
+
+  takeScreenshot(): void {
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { NgxCameraModule } from 'ngx-camera';
+import { ButtonModule } from 'primeng/button';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxCameraModule,
+    ButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
